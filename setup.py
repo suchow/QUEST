5ff991d2f28b35a3ee9e40a71ebfb032df08e976
Jordan Suchow <suchow@post.harvard.edu>
2019-12-08 11:15:45 -0500

    Initial commit of Vision Egg's QUEST

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='quest',
+    version='1.2',
+    description="QUEST algorithm for estimating threshold",
+    author="Denis Pelli",
+    maintainer="Jordan W. Suchow",
+    license="BSD",
+    packages=[''],
+    package_dir={'': 'src'},
+)
